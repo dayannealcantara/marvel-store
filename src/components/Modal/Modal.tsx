f6f45e2fd33b17772b401e13571785f5f7b21e74
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-04 16:32:52 -0300

    feat:modal with hero details

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import * as S from './modal.styles';
+
+type ModalProps = {
+  onClose?: () => void;
+};
+
+export const Modal = ({ onClose }: ModalProps) => {
+  return (
+    <S.ModalLayout>
+      <S.ContainerModal>
+        <S.ContainerClose>
+          <h1>3-D MAN</h1>
+          <S.CloseModal onClick={onClose}>
+            <Image src="/close.png" alt="3d Marvel" width={20} height={20} />
+          </S.CloseModal>
+        </S.ContainerClose>
+        <S.ModalContent>
+          <S.Teste>
+            <Image src="/3dMan.jpg" alt="3d Marvel" width={400} height={300} />
+            <div>
+              <div>
+                <h3>ID</h3>
+                <p>1011334</p>
+              </div>
+              <div>
+                <h3>DESCRIÇÃO</h3>
+                <p>No description</p>
+              </div>
+              <div>
+                <h3>DATA DE MODIFICAÇÃO</h3>
+                <p>2014-04-29T14:18:17-0400</p>
+              </div>
+              <div>
+                <h3>ÚLTIMAS HQS</h3>
+                <p>Avengers: The Initiative (2007 - 2010) </p>
+                <p>Deadpool (1997 - 2002)</p>Deadpool (1997 - 2002)
+                <p> Marvel Premiere (1972 - 1981)</p>
+              </div>
+            </div>
+          </S.Teste>
+          <S.ContainerAdd>
+            <S.AddItem>Adicionar</S.AddItem>
+          </S.ContainerAdd>
+        </S.ModalContent>
+      </S.ContainerModal>
+    </S.ModalLayout>
+  );
+};
---
+++ src/components/Modal/modal.styles.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const ContainerClose = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+export const CloseModal = styled.button`
+  cursor: point;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  width: 32px;
+  height: 32px;
+`;
+export const ContainerAdd = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 100px;
+`;
+export const AddItem = styled.button`
+  font-size: 20px;
+  cursor: pointer;
+  border: 2px solid #fb1;
+  border-radius: 0.4rem;
+  background: #fea;
+  color: #764701;
+  min-width: 8rem;
+  padding: 0.8rem 1.2rem;
+  box-sizing: border-box;
+  transition: 0.1s;
+  &:hover,
+  &:focus {
+    outline: none;
+    background: #fb1;
+  }
+  &:disabled {
+    opacity: 0.5;
+    cursor: wait;
+  }
+`;
+
+export const ContainerModal = styled.div`
+  background: white;
+  color: black;
+  width: 60%;
+  height: 100%;
+  border-radius: 20px 0 0 20px;
+`;
+export const ModalLayout = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const Teste = styled.div`
+  overflow: hidden;
+  padding: 20px 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
