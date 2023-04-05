c168262615d3bae7aafd47a9b97e9e625a3b60e1
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-05 15:50:59 -0300

    feat:styling home page

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,81 +1,84 @@
 import styled from 'styled-components';
+import { darken, lighten } from "polished";
 
-export const WrapperHeader = styled.header`
+export const Container = styled.div`
   display: flex;
-  width: 100%;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6rem;
+  min-height: 100vh;
 `;
 
-export const WrapperInput = styled.div`
-  display: flex;
-  justify-content: center;
-`;
 
-export const WrapperList = styled.div`
+export const ProductList = styled.ul`
   display: grid;
-  justify-content: center;
-  width: 100%;
-  grid-template-columns: repeat(4, 20%);
-  gap: 25px;
-`;
-export const Table = styled.table`
-  width: 100%;
-  min-height: 26vh;
-  border-collapse: collapse;
-  overflow: hidden;
-`;
-export const Thead = styled.thead`
-  width: 100%;
-  position: sticky;
-  z-index: 100;
-  border-bottom: 2px solid red;
-  tr {
-    font-size: 1.5rem;
-  }
-`;
-export const ThProduct = styled.th`
-  text-align: left;
-  width: 30vw;
-  padding-bottom: 1rem;
-`;
-export const Th = styled.th`
-  width: 12vw;
-  text-align: left;
-  padding-bottom: 1rem;
-`;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
 
-export const TbodyTR = styled.tr`
-  position: sticky;
-  z-index: 100;
-  border-bottom: 3px solid #767070;
-`;
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
 
-export const TdDelete = styled.span`
-  text-align: center;
-  cursor: pointer;
-  font-size: 1.5rem;
-  margin: 0 1rem;
-  padding: 0.2rem 0.5rem;
-  border-radius: 0.3rem;
-  &:hover {
-    background: #76707059;
-  }
-`;
-export const TdImage = styled.td`
-  width: 8%;
-  height: auto;
-  padding: 0.8rem 0;
-`;
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
 
-export const ButtonTd = styled.button`
-  width: 2.5em;
-  text-align: center;
-  background: transparent;
-  cursor: pointer;
-  margin: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  border-radius: 0.3rem;
-  &:hover {
-    background: #76707059;
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      color: #333;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      overflow: hidden;
+      margin-top: auto;
+
+      display: flex;
+      align-items: center;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.06, "#7159c1")};
+      }
+
+      &:disabled {
+        background: ${lighten(0.25, "#7159c1")};
+        cursor: not-allowed;
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: rgba(0, 0, 0, 0.1);
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
   }
 `;
