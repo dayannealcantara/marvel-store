bd7d607df1af4c01d649198821e39224b3269271
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-05 15:42:55 -0300

    feat:removing unused files

--- src/components/Header/header.style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const ContainerHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background: rgb(25, 25, 32);
-`;
