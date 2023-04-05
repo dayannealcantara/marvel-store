a52d5190ff2de5bfbdc10d9fb9e24a2f53300c74
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-05 15:51:39 -0300

    feat:tweaks to global styling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,34 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+
+export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
   }
-  @media (max-width: 1080px) {
-    html {
-      font-size: 93.75%;
-    }
-  }
-  @media (max-width: 720px) {
-    html {
-      font-size: 87.5%;
-    }
-  }
+
   body {
-    background: #ffffff;
+    background: rgb(25, 25, 32);
+    background: linear-gradient(162deg, rgba(1,1,17,1) 10%, rgba(24,24,45,0.9864320728291317) 35%, rgba(5,5,14,0.98) 97%);
+
     -webkit-font-smoothing: antialiased;
   }
+
   body, input, button {
     font: 14px Roboto, sans-serif;
   }
+
   #root {
     max-width: 1020px;
     margin: 0 auto;
     padding: 0 20px 50px;
   }
+
   button {
     cursor: pointer;
   }
