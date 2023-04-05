06d7faab00257d4ab09447fd34d4073658ec3a36
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-04 23:52:33 -0300

    feat:adjust

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import { GlobalStyle } from 'styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,48 @@
 import type { NextPage } from 'next';
-
-import styles from '../styles/page.module.css';
-
+import { useEffect } from 'react';
+import md5 from 'md5';
 import cardMarvel from '../mockups/cardMarvel.json';
-import { List } from '@/components/Lista';
-import { Header } from '@/components/Header/Header';
-import { Input } from '@/components/Input/Input';
+
+import { Header } from 'components/Header/Header';
+import { Input } from 'components/Input/Input';
+import api from 'services/api';
+import * as S from './styles';
+import { Card } from 'components/Card/Card';
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+const privateKey = 'b93a1d42bf083c90b758ab6f00f39879631ba229';
+const time = Number(new Date());
+const hash = md5(time + privateKey + publicKey);
 
 const Home: NextPage = () => {
+  console.log('hasf', hash);
+  const getComics = async () => {
+    try {
+      const response = await api.get(
+        `comics?ts=${time}&apikey=${publicKey}&hash=${hash}&offset=0`
+      );
+      console.log(response);
+    } catch {
+      console.log('error');
+    }
+  };
+
+  useEffect(() => {
+    getComics();
+  }, []);
+
   return (
     <>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          padding: ' 2rem 5rem'
-        }}
-      >
+      <S.WrapperHeader>
         <Header />
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: '30px'
-          }}
-        >
+        <S.WrapperInput>
           <Input />
-        </div>
-      </div>
-      <div className={styles.cardMarvel}>
+        </S.WrapperInput>
+      </S.WrapperHeader>
+      <S.WrapperList>
         {cardMarvel.map((card) => (
-          <List key={card.name} name={card.name} imageUrl={card.imageUrl} />
+          <Card key={card.name} name={card.name} imageUrl={card.imageUrl} />
         ))}
-      </div>
+      </S.WrapperList>
     </>
   );
 };
