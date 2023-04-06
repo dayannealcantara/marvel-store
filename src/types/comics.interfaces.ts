2cec3451235303986d45c8063655d00ae28f4d15
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-06 20:35:06 -0300

    feat: adjust in the interface of the cards and products of the added ones

--- src/types/comics.interfaces.ts
+++ src/types/comics.interfaces.ts
@@ -3,12 +3,28 @@ interface IPrice {
 }
 
 export interface IComic {
-  id: number;
+  id: string;
   title: string;
   prices: IPrice[];
   amount: number;
+  description?: string;
+  pageCount: number;
   thumbnail: {
     path: string;
     extension: string;
   };
+}
+
+export interface ICreator {
+  fullName: string;
+  image: string;
+}
+
+export interface IComicSelected {
+  id?: string;
+  title: string;
+  description?: string;
+  pageCount?: number;
+  image: string;
+  creators?: ICreator[];
 }
