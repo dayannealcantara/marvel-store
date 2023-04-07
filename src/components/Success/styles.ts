aaad8ac7803cf63b6347882882787f5db838782f
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-07 11:28:50 -0300

    faet:purchase success component

---
+++ src/components/Success/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import { darken, lighten } from 'polished';
+import Link from 'next/link';
+
+export const Container = styled.section`
+  min-height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  @media (max-width: 560px) {
+    min-height: 400px;
+  }
+
+  svg {
+    font-size: 260px;
+    color: #f3f3f6;
+    position: absolute;
+  }
+`;
+
+export const Wrapper = styled.div`
+  z-index: 1;
+  text-align: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 56px;
+`;
+export const Description = styled.p`
+  margin: 12px 0 28px;
+  font-size: 16px;
+`;
+
+export const StartShopping = styled(Link)`
+  background: #7159c1;
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+  padding: 12px 20px;
+  font-weight: bold;
+  text-transform: uppercase;
+  transition: background 180ms ease-in-out;
+  text-decoration: none;
+  display: inline-block;
+
+  &:hover {
+    background: ${lighten(0.04, '#7159c1')};
+  }
+
+  &:active {
+    background: ${darken(0.04, '#7159c1')};
+  }
+`;
