fb3de6a7f36a4ef335554504cbb22f1044330edf
Dayanne Alcantara <dayannealcantara@ioasys.com.br>
2023-04-08 11:46:43 -0300

    feat:styled componentet configuration tweak

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  experimental: {
-    appDir: false
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
   }
 };
 
